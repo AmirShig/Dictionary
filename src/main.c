36d93d7cd2f1291ec86fa80d9a247fd89bf3896f
AmirShig <shigapov_amir@mail.ru>
2023-02-23 03:52:13 +0300

    пробник базы данных с файлом .csv

--- src/main.c
+++ src/main.c
@@ -2,17 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Dictionary {
-    char words[20];
-    char translate[20];
+#define NMAX 100
+
+typedef struct Dictionary {
+    char word[256];
+    char translate[256];
     int status;
-};
+} Dictionary;
+
+Dictionary Base[NMAX];
 
 int main() {
-    struct Dictionary a = {'hello', 'привет', 1};
-    printf("%s - %s; Слово изучено на %d%%", a.words, a.translate, a.status);
+    Dictionary a;
+    char buffer_word[256] = "hello";
+    // scanf("%s", &buffer_word[256]);
+    // fflush(stdin);
+    // fgets(buffer_word, 256, stdin);
+    strcpy(a.word, buffer_word);
+    char buffer_translate[256] = "привет";
+    // scanf("%s", &buffer_translate[256]);
+    // fgets(buffer_translate, 256, stdin);
+    strcpy(a.translate, buffer_translate);
+    a.status = 0;
+    printf("%s - %s;\nСлово изучено на %d%% из 100%%", a.word, a.translate, a.status);
+    // printf("%s - %s; Слово изучено на %d%%", a.words, a.translate, a.status);
 
-
-    // printf("Hello world!");
     return 0;
 }
--- src/practice.c
+++ src/practice.c
@@ -1,21 +1,101 @@
 #include <stdio.h>
+#include <string.h>
 
-struct Point {
-    int x;
-    int y;
-};
+void read();
+void add();
 
-struct Rect1 {
-    struct Point lt;
-    struct Point rb;
-};
-int main() {
-    struct Rect1 a = {{7, 5}, {5, 3}};
-    // a.lt.x = -7;
-    // a.lt.y = 5;
-    // a.rb.x = 5;
-    // a.rb.y = -3;
-    printf("%d %d %d %d\n", a.lt.x, a.lt.y, a.rb.x, a.rb.y);
-
+int main()
+{   
+    int mode;
+    scanf("%d", &mode);
+    while(getchar() != '\n');
+    switch (mode)
+    {
+    case 1:   
+        read();
+        break;
+    case 2: 
+        add();
+        break;
+    default:
+        break;
+    }
     return 0;
+}
+
+void read(){
+    
+    FILE* fp = fopen("words.csv", "r");
+ 
+    if (!fp)
+        printf("Can't open file\n");
+ 
+    else {
+     
+        char buffer[1024];
+ 
+        int row = 0;
+        int column = 0;
+ 
+        while (fgets(buffer,
+                     1024, fp)) {
+            column = 0;
+            row++;
+ 
+            char* value = strtok(buffer, ", ");
+ 
+            while (value) {
+                // Column 1
+                if (column == 0) {
+                    printf("Eng :");
+                }
+ 
+                // Column 2
+                if (column == 1) {
+                    printf("\tRus :");
+                }
+ 
+                // Column 3
+                if (column == 2) {
+                    printf("\tStatus :");
+                }
+ 
+                printf("%s", value);
+                value = strtok(NULL, ", ");
+                column++;
+            }
+ 
+            printf("\n");
+        }
+        fclose(fp);
+    }
+}
+
+void add(){
+	
+	FILE* fp = fopen("words.csv", "a+");
+
+	char eng[50], rus[50];
+	int status;
+
+	if (fp != NULL) {
+		
+        printf("\nEnter english word\n");
+        scanf("%s", eng);
+        printf("\n%s\nEnter translate\n", eng);
+        scanf("%s", rus);
+        printf("\nEnter learn-status\n");
+        scanf("%d", &status);
+
+        
+        fprintf(fp, "%s, %s, %d\n", eng,
+                rus, status);
+
+        printf("\nNew word added");
+
+        fclose(fp);
+	} else {
+        
+		printf("Can't open file\n");
+    }	
 }
