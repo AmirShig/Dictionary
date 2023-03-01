8c5b6340979ced3ac1be37f46999a3d11a49c4e3
AmirShig <shigapov_amir@mail.ru>
2023-03-01 09:15:54 +0300

    оптимизация

--- src/pr_txt.c
+++ src/pr_txt.c
@@ -30,23 +30,39 @@ void eng(char *search) {
     // printf("Here");
     // printf("%s", search);
     FILE *words=fopen("eng_rus/ENRUS.txt", "r");
-    char row[SIZE], translate[SIZE];
+    char row[SIZE]/*, s2[SIZE]*/, translate[SIZE];
     int err = 0, status = 0, flag = 0;
     while(fgets(row, SIZE, words)) {
+        // for(int i = 0; search[i] != '\n'; s2[i] = search[i], i++);
+        // printf("%s запрошенное слово ", search);
         // поиск совпадений (необязательно 1к1)
         if (strstr(row, search)) {
-            // printf("search: %s\nres: %sСравнение строк: %d\n", search, row, strcmp(row, search));
+            // printf("strcmp = %d\n", strcmp(row, search));
             if (strcmp(row, search) == 13 && !status) {
                 flag = 1;
                 fgets(translate, SIZE, words);
-                printf("\t%sПеревод: %s\n", row, translate);
-            } else if (!flag) {
+                for(int i = 0; row[i] != '\n'; printf("%c", row[i]), i++);
+                // printf("%s запрошенное слово ", row2);
+                printf("\t--->  %s\n",  translate);
+            } 
+            // else if (!flag) {
+            //     if (!status) printf("Пожалуйста, введите запрос более конкретно.\nВозможно, вы имели ввиду:\n");
+            //     // fgets(translate, SIZE, words);
+            //     printf("%s", row);
+            //     status = 1;
+            // }
+            err = 1;
+        }
+    }
+    if (!flag && err) {
+        if(fseek(words, SEEK_CUR, SEEK_SET)) perror("Ошибка переноса курсора: ");
+        while(fgets(row, SIZE, words)){
+            if (strstr(row, search)) {
                 if (!status) printf("Пожалуйста, введите запрос более конкретно.\nВозможно, вы имели ввиду:\n");
                 // fgets(translate, SIZE, words);
                 printf("%s", row);
                 status = 1;
             }
-            err = 1;
         }
     }
     if (!err) printf("Введенное слово отсутствует в базе.\n");
